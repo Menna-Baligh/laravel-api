ee00160936ba460eb708a03ab1fd7f99f9bc5a0b
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-14 22:27:24 +0300

    add fillable to model and do the postcontroller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -21,7 +22,15 @@ Route::get('/test', function (){
 
 //* CRUD on posts
 //* 1. get all (GET) /api/posts
+// Route::get('/posts',[PostController::class, 'index']);
 //* 2. create a post (POST) /api/posts
+// Route::post('/posts', [PostController::class, 'store']);
 //* 3. get one post (GET) /api/posts/{id}
+// Route::get('/posts/{id}', [PostController::class, 'show']);
 //* 4. update a post (PUT/PATCH) /api/posts/{id}
+// Route::put('/posts/{id}', [PostController::class, 'update']);
 //* 5. delete a post (DELETE) /api/posts/{id}
+// Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+//* shorthand for the previous routes
+Route::apiResource('/posts', PostController::class);
